6a7faeea84693aaac5fea597575611a82c611e99
ballardcharles <73306975+ballardcharles@users.noreply.github.com>
2021-03-24 16:41:17 -0400

    added filtering to console log.

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -14,15 +14,35 @@ tableData.forEach((alienSightings) => {
     });
   });
 
-var newDate = filter.on("change", function(){
-    d3.event.target.value;
-})
+filter.on("change", runFilter)
 
-function selectDate(date) {
-    return date.datetime === newDate
+function runFilter() {
+    // d3.event.preventDefault();
+
+    var inputDate = d3.select("#datetime")
+
+    var newDate = inputDate.property("value")
+
+    var filterDate = tableData.filter(sighting => sighting.datetime === newDate)
+
+    console.log(filterDate)
+
+    filterDate.forEach((alienSightings) => {
+        var row = tbody.append("tr");
+        Object.entries(alienSightings).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
 }
 
-var selectedDate = tableData.filter(selectDate)
 
-console.log(selectedDate)
+
+// function selectDate(date) {
+//     return date.datetime === newDate
+// }
+
+// var selectedDate = tableData.filter(selectDate)
+
+// console.log(selectedDate)
 
