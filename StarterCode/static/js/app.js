708bb7edcbf48c89b19a1b0049f23cb1796f34aa
ballardcharles <73306975+ballardcharles@users.noreply.github.com>
2021-03-24 06:12:30 -0400

    added javascript to display alien sighting data as table.

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,14 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select("tbody")
+
+// console.log(tableData)
 
 // YOUR CODE HERE!
+tableData.forEach((alienSightings) => {
+    var row = tbody.append("tr");
+    Object.entries(alienSightings).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
